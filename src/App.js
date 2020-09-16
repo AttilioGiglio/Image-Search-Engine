3b17902881316e5eaab0a4e42fb36bfdd4b19451
attiliogiglio <attilio.g93@gmail.com>
2020-09-16 15:09:55 -0300

    fix to pagination

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
 
   const [state, setState] = useState('');
   const [images, setImage] = useState([]);
-  const [paginaActual, setImagenActual] = useState(1);
-  const [totalPaginas, setTotalPaginas] = useState(1);
+  const [paginaActual, setPaginaActual] = useState(6);
+  const [totalPaginas, setTotalPaginas] = useState(5);
 
 
   useEffect(() => {
@@ -21,11 +21,25 @@ function App() {
       const resultado = await respuesta.json()
       setImage(resultado.hits);
       // calcular total de paginas
-      const calcularTotalPaginas = Math.ceil(resultado.totalHits/imagenesPorPagina);
+      const calcularTotalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
       setTotalPaginas(calcularTotalPaginas);
     }
     consultarAPI();
   }, [state])
+
+
+  const paginaAnterior = () =>{
+    const nuevaPaginaActual = paginaActual - 1;
+    if(nuevaPaginaActual === 0){return;}
+    setPaginaActual(nuevaPaginaActual)
+  }
+
+  const paginaSiguiente = () =>{
+    const nuevaPaginaActual = paginaActual + 1;
+    if(nuevaPaginaActual > totalPaginas){return;}
+    setPaginaActual(nuevaPaginaActual)
+  }
+
 
   return (
     <div className="container">
@@ -42,6 +56,20 @@ function App() {
           images={images}
         />
       </div>
+      <button
+        type='button'
+        className='bbtn btn-info mr-1'
+        onClick={paginaAnterior}
+      >
+        &laquo; Anterior
+      </button>
+      <button
+        type='button'
+        className='bbtn btn-info mr-1'
+        onClick={paginaSiguiente}
+      >
+        Siguiente &raquo;
+      </button>
     </div>
   );
 }
