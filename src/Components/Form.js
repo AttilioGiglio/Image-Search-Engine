9a146733601e9e85ddb0b3321ffbf2bd709d1bf8
attiliogiglio <attilio.g93@gmail.com>
2020-09-16 14:16:26 -0300

    adding validation form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Error from './Error';
 
 const Form = () => {
+
+    const [search, setSearch] = useState('');
+    const [error, setError] = useState(false);
+
+    const searchImage = e => {
+        e.preventDefault();
+        // Validar
+        if (search.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
+    }
+
     return (
-        <form>
-           <div className='row'>
+        <form onSubmit={searchImage}>
+            <div className='row'>
                 <div className='form-group col-md-8'>
                     <input
-                    type='text'
-                    className='form-control form-control-lg'
-                    placeholder='Busca una imagen, ejemplo: futbol o cafe'
+                        type='text'
+                        className='form-control form-control-lg'
+                        placeholder='Busca una imagen, ejemplo: futbol o cafe'
+                        onChange={e => setSearch(e.target.value)}
                     />
                 </div>
                 <div className='form-group col-md-4'>
                     <input
-                    type='submit'
-                    className='btn btn-lg btn-danger btn-block'
-                    placeholder='Buscar'
+                        type='submit'
+                        className='btn btn-lg btn-danger btn-block'
+                        value='Buscar'
                     />
-                </div>   
-            </div> 
+                </div>
+            </div>
+            {error ? <Error mensaje='Agrega un termino de busqueda, porfavor' /> : null}
         </form>
     )
 }
